78e5a09d72c1899949a95268d8bb01dc071a1d04
HB <1084109450@qq.com>
2022-03-10 21:25:04 +0800

    master third commit

--- src/main/java/com/hb/git/GitTest.java
+++ src/main/java/com/hb/git/GitTest.java
@@ -1,13 +1,10 @@
 package com.hb.git;
-/**
- * @author 黄彬
- * @date 2022-03-10 21:04
- * @vesion V1.0
- */
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
+        System.out.println("master test!");
     }
 }
