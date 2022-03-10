86eb7bb98e744aa23454a8ebe3e0b401ce9f7059
HB <1084109450@qq.com>
2022-03-10 21:23:38 +0800

    hot-fix commit second

--- src/main/java/com/hb/git/GitTest.java
+++ src/main/java/com/hb/git/GitTest.java
@@ -1,9 +1,5 @@
 package com.hb.git;
-/**
- * @author 黄彬
- * @date 2022-03-10 21:04
- * @vesion V1.0
- */
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1!");
