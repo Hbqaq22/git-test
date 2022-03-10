3ac9dfa4b43b44ea0e2bef5fbeadbcbd89349f27
HHH <1084109450@qq.com>
2022-03-10 15:22:47 +0000

    update src/main/java/com/hb/git/GitTest.java.

--- src/main/java/com/hb/git/GitTest.java
+++ src/main/java/com/hb/git/GitTest.java
@@ -2,13 +2,7 @@ package com.hb.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git1!");
-        System.out.println("hello git2!");
-        System.out.println("hello git3!");
-        System.out.println("master test!");
-        System.out.println("hot-fix test!");
-        System.out.println("push test!");
-        System.out.println("pull test!");
         System.out.println("gitee test");
+        System.out.println("gitee test1");
     }
 }
