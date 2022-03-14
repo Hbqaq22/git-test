384c82383d6d6f684547404da5ef6c1cab5dfed6
HB <1084109450@qq.com>
2022-03-14 23:48:34 +0800

    gitee commit first

--- src/main/java/com/hb/git/GitTest.java
+++ src/main/java/com/hb/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("gitee test");
         System.out.println("gitee test1");
+        System.out.println("gitee test1");
     }
 }
